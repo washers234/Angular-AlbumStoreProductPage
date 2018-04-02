d099d2642726a3137aa6ba2377f8875c533e2e6f
washers234 <38015496+washers234@users.noreply.github.com>
2018-04-02 21:49:04 +0100

    r

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -7,12 +7,12 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class ProductService {
 
-  private _albumURL = '../assets/album.json';
+  private _albumUrl = '../assets/album.json';
 
   constructor(private _http: Http) { }
 
   getAlbum (id: number){
-    return this._http.get(this._albumURL).map((response) =>
+    return this._http.get(this._albumUrl).map((response) =>
     response.json());
   }
 }
